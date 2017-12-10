ffb0a2df3fcc1776120e0038339c678d5d5088a5
Satendra1729 <satendra1729@gmail.com>
2017-12-10 19:41:16 +0530

    angualr template setyp stript

---
+++ angularDoc.sh
@@ -0,0 +1,8 @@
+ng new mcaPortal4
+cd mcaPortal4
+npm install --save @ng-bootstrap/ng-bootstrap
+#npm install bootstrap@4.0.0-alpha.6
+npm install @angular/common@latest @angular/compiler@latest @angular/compiler-cli@latest @angular/core@latest @angular/forms@latest @angular/http@latest @angular/platform-browser@latest @angular/platform-browser-dynamic@latest @angular/platform-server@latest @angular/router@latest @angular/animations@latest typescript@latest --save
+npm install --save @angular/material @angular/cdk
+npm install --save @angular/animations
+npm install --save hammerjs
